67b4f7631f0e88681d20a3106a64f0834deeb350
AK <andrejs.kolesnikovs.lv@gmail.com>
2019-10-13 15:02:22 +0300

    PageObject

---
+++ src/test/java/pages/BaseFunc.java
@@ -0,0 +1,26 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseFunc {
+
+    private WebDriver driver;
+
+    public BaseFunc() {
+        System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    public void goToUrl(String url) {
+        if (url.startsWith("http://") || url.startsWith("https://")) {
+
+        } else {
+            url = "http://" + url;
+        }
+        driver.get(url);
+
+    }
+
+}
